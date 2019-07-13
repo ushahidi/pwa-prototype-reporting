72766ffd02d8bec7d2899e9374f19de63e2073e0
Nidhi Kumari <nidhi1691996@outlook.com>
2019-07-13 23:07:12 +0530

    From handling with api

--- components/form.js
+++ components/form.js
@@ -8,65 +8,80 @@ export default class Form extends React.Component {
     textField: ""
   };
 
+  // Make state targets equal to the value of input fields
   change = e => {
     this.setState({
       [e.target.name]: e.target.value
     });
   };
-
+  
+  // Handler for when the submit button is pressed
   onSubmit = e => {
     e.preventDefault();
+
+    console.log(this.state);
+
+    // Make a post request to Ushahidi sever
+    fetch("https://rominacsvsms.ushahidi.io/posts/create/14", {
+      method: "POST",
+      body: JSON.stringify(this.state),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(`Success!!! \n ${data}`);
+      })
+      .catch(error => {
+        console.log(error);
+      });
 
     this.setState({
       title: "",
       description: "",
       textField: ""
     });
-    this.props.onChange({
-      title: "",
-      Description: "",
-      textField: ""
-    });
   };
 
   render() {
     return (
-      
       <form>
-      <div>
-        <label for = "title" >Title</label> 
-        <input
-          name="title"
-          placeholder="Title"
-          value={this.state.title}
-          onChange={e => this.change(e)}
-        />
-        </div>
-       
         <div>
-        <label for = "description" >Description</label> 
-        <input
-          name=""description
-          
-          placeholder="Description"
-          value={this.state.description}
-          onChange={e => this.change(e)}
-        />
+          <label for="title">Title</label>
+          <input
+            name="title"
+            placeholder="Title"
+            value={this.state.title}
+            onChange={e => this.change(e)}
+          />
         </div>
+
         <div>
-        <label for = "textField" >Text Field</label> 
-        <input
-          name="textField"
-          placeholder="Text Field"
-          value={this.state.textField}
-          onChange={e => this.change(e)}
-        />
+          <label for="description"> Description</label>
+          <input
+            name="description"
+            placeholder="Description"
+            value={this.state.description}
+            onChange={e => this.change(e)}
+          />
         </div>
         <div>
-        <button onClick={e => this.onSubmit(e)}>Submit</button>
+          <label for="textField">Text Field</label>
+          <input
+            name="textField"
+            placeholder="Text Field"
+            value={this.state.textField}
+            onChange={e => this.change(e)}
+          />
+        </div>
+        <div>
+          <button onClick={e => this.onSubmit(e)}>Submit</button>
         </div>
       </form>
-      
     );
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,12 @@
-import React from 'react'
-import Link from 'next/link'
-import Head from '../components/head'
-import Form from '../components/form'
-
-
-
+import React from "react";
+import Link from "next/link";
+import Head from "../components/head";
+import Form from "../components/form";
 
 const Home = () => (
   <div>
-    <Head title="Home" />
-    <Form/>
-
-    <div className="hero">
-      
-
-      <div className="row">
-       
-        
-      </div>
-    </div>
-
-    
+    <Form />
   </div>
-)
+);
 
-export default Home
+export default Home;
