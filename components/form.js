73a305fadd9fed0f4cb6faacc9d459c5c8e56966
Nidhi Kumari <nidhi1691996@outlook.com>
2019-07-19 18:21:17 +0530

    token added

--- components/form.js
+++ components/form.js
@@ -1,23 +1,24 @@
-import React from "react";
-import Link from "next/link";
-
+import React from 'react';
+import Link from 'next/link';
+ 
 export default class Form extends React.Component {
-  constructor() {
-    super();
-    this.state = { formFields: [], formData: {} };
-}
-  // Make state targets equal to the value of input fields
-  change = e => {
-    this.setState({
-      formData: {
-        ...this.state.formData,
-      [e.target.name]: e.target.value
-      }
-    });
-  };
-
-      // Fetch the data form fields from API, use it in state
-      componentDidMount() {
+    constructor() {
+        super();
+        this.state = { token: '', formFields: [], formData: {} };
+    }
+ 
+    // Make state targets equal to the value of input fields
+    change = e => {
+        this.setState({
+            formData: {
+                ...this.state.formData,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+ 
+    // Fetch the data form fields from API, use it in state
+    componentDidMount() {
         fetch(
             'https://rominacsvsms.api.ushahidi.io/api/v3/forms/14/attributes?order=asc&orderby=priority'
         )
@@ -27,62 +28,91 @@ export default class Form extends React.Component {
                     formFields: data.results
                 });
             });
+ 
+        if (!this.state.token) {
+            fetch('https://rominacsvsms.api.ushahidi.io/oauth/token', {
+                method: 'POST',
+                body: JSON.stringify({
+                    scope: '*',
+                    client_secret: '35e7f0bca957836d05ca0492211b0ac707671261',
+                    client_id: 'ushahidiui',
+                    grant_type: 'password',
+                    password: 'hellohello',
+                    username: 'menidhikjha@gmail.com'
+                }),
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authentication: 'large-auth-string'
+                }
+            })
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    this.setState({ token: data });
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
     }
-  
-  // Handler for when the submit button is pressed
-  onSubmit = e => {
-    e.preventDefault();
-
-    // Make a post request to Ushahidi sever
-    fetch("https://rominacsvsms.ushahidi.io/posts/create/14", {
-      method: "POST",
-      body: JSON.stringify(this.state.formData),
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      }
-    })
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        console.log(`Success!!! \n ${data}`);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-
-      this.setState({
-        formData: {}
-    });
-};
-
- // Create an array of elemets to be rendered in the form
- FormInputs = props => {
-  return props.fields.map(field => {
-      return (
-          <div key={field.key}>
-              <label htmlFor={field.label}>{field.label}</label>
-              <input
-                  type={field.input}
-                  name={field.label}
-                  placeholder={`Enter the ${field.label}`}
-                  value={this.state.formData[`${field.label}`]}
-                  onChange={e => this.change(e)}
-              />
-          </div>
-      );
-  });
-};
-
-render() {
-  return (
-      <form>
-          <this.FormInputs fields={this.state.formFields} />
-          <div>
-              <button onClick={e => this.onSubmit(e)}>Submit</button>
-          </div>
-      </form>
-  );
-}
+ 
+    // Handler htmlFor when the submit button is pressed
+    onSubmit = e => {
+        e.preventDefault();
+ 
+        // Make a post request to Ushahidi sever
+        fetch('https://rominacsvsms.api.ushahidi.io/api/v3/posts', {
+            method: 'POST',
+            body: JSON.stringify(this.state.formData),
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authentication: 'Bearer ' + this.state.token
+            }
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(`Success!!! \n ${data}`);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+ 
+        this.setState({
+            formData: {}
+        });
+    };
+ 
+    // Create an array of elemets to be rendered in the form
+    FormInputs = props => {
+        return props.fields.map(field => {
+            return (
+                <div key={field.key}>
+                    <label htmlFor={field.label}>{field.label}</label>
+                    <input
+                        type={field.input}
+                        name={field.label}
+                        placeholder={`Enter the ${field.label}`}
+                        value={this.state.formData[`${field.label}`]}
+                        onChange={e => this.change(e)}
+                    />
+                </div>
+            );
+        });
+    };
+ 
+    render() {
+        return (
+            <form>
+                <this.FormInputs fields={this.state.formFields} />
+                <div>
+                    <button onClick={e => this.onSubmit(e)}>Submit</button>
+                </div>
+            </form>
+        );
+    }
 }
