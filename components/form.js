ec740b8b5d1c8f302f0ff5eb26b4f062a6dc1711
Nidhi Kumari <nidhi1691996@outlook.com>
2019-07-29 18:49:14 +0530

    Formjs updated

--- components/form.js
+++ components/form.js
@@ -1,152 +1,178 @@
-import React from "react";
-import Link from "next/link";
-import { Person } from "./Users";
+import React from 'react';
+import Link from 'next/link';
+import { Person } from './Users';
 
-const baseUrl = "https://rominacsvsms.api.ushahidi.io";
+const baseUrl = 'https://rominacsvsms.api.ushahidi.io';
 
 export default class Form extends React.Component {
-  constructor() {
-    super();
-    this.state = { token: "", formFields: [], formData: {} };
-  }
-
-  // Make state targets equal to the value of input fields
-  change = e => {
-    this.setState({
-      formData: {
-        ...this.state.formData,
-        [e.target.name]: e.target.value
-      }
-    });
-  };
-
-  componentDidMount() {
-    // Fetch the data form fields from API, use it in state
-    let formFields = JSON.parse(localStorage.getItem("Form Fields"));
-    if (!(Array.isArray(formFields) && formFields.length)) {
-      fetch(baseUrl + "/api/v3/forms/14/attributes?order=asc&orderby=priority")
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error("Field fetching response is not ok.");
-          }
-        })
-        .then(data =>
-          this.setState({
-            formFields: data.results
-          })
-        )
-        .catch(err => console.log(err));
-    } else {
-      this.setState({
-        formFields: formFields
-      });
+    constructor() {
+        super();
+        this.state = {
+            token: {
+                access_token: '',
+                expires_in: 0,
+                refresh_token: '',
+                token_type: ''
+            },
+            formFields: [],
+            formData: {}
+        };
     }
 
-    let token = JSON.parse(localStorage.getItem("Bearer Token"));
+    // Make state targets equal to the value of input fields
+    change = e => {
+        this.setState({
+            formData: {
+                ...this.state.formData,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
 
-    if (!token) {
-      fetch(baseUrl + "/oauth/token", {
-        method: "POST",
-        body: JSON.stringify({
-          scope: "*",
-          client_secret: "35e7f0bca957836d05ca0492211b0ac707671261",
-          client_id: "ushahidiui",
-          grant_type: "password",
-          password: Person.password,
-          username: Person.userName
-        }),
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: "large-auth-string"
+    isTokenValid() {
+        let isValid = false;
+        const currentTime = new Date().getTime();
+        const tokenFetchTime = this.state.token.fetching_time;
+        const expirationTime = this.state.token.expires_in;
+        if (currentTime < tokenFetchTime + expirationTime) {
+            valid = true;
         }
-      })
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error("Token fetching response is not ok.");
-          }
-        })
-        .then(data => this.setState({ token: data }))
-        .catch(error => console.log(error));
-    } else {
-      this.setState({ token: token });
+        return isValid;
     }
-  }
 
-  componentDidUpdate(prevProps, prevState) {
-    localStorage.setItem("Form Fields", JSON.stringify(this.state.formFields));
-    localStorage.setItem("Bearer Token", JSON.stringify(this.state.token));
-  }
-  {
-  var object = {value: "value", timestamp: new Date().getTime()}
-localStorage.setItem("Form Fields", JSON.stringify(this.state.token));
+    getToken() {
+        let token = JSON.parse(localStorage.getItem('Bearer Token'));
+        if (!token || this.isTokenValid()) {
+            fetch(baseUrl + '/oauth/token', {
+                method: 'POST',
+                body: JSON.stringify({
+                    scope: '*',
+                    client_secret: '35e7f0bca957836d05ca0492211b0ac707671261',
+                    client_id: 'ushahidiui',
+                    grant_type: 'password',
+                    password: Person.password,
+                    username: Person.userName
+                }),
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: 'large-auth-string'
+                }
+            })
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error('Token fetching response is not ok.');
+                    }
+                })
+                .then(data =>
+                    this.setState({
+                        token: { ...data, fetching_time: new Date().getTime() }
+                    })
+                )
+                .catch(error => console.log(error));
+        } else {
+            this.setState({ token: token });
+        }
+    }
 
-var object = JSON.parse(localStorage.getItem("Form Fields")),
-  dateString = object.timestamp,
-  Time = new Date().getTime().toString();
+    getFormFields() {
+        // Fetch the data form fields from API, use it in state
+        let formFields = JSON.parse(localStorage.getItem('Form Fields'));
+        if (!(Array.isArray(formFields) && formFields.length)) {
+            fetch(
+                baseUrl +
+                    '/api/v3/forms/14/attributes?order=asc&orderby=priority'
+            )
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error('Field fetching response is not ok.');
+                    }
+                })
+                .then(data =>
+                    this.setState({
+                        formFields: data.results
+                    })
+                )
+                .catch(err => console.log(err));
+        } else {
+            this.setState({
+                formFields: formFields
+            });
+        }
+    }
 
-compareTime(dateString, Time); 
-  }
+    componentDidMount() {
+        this.getFormFields();
+        this.getToken();
+    }
 
-  // Handler html For when the submit button is pressed
-  onSubmit = e => {
-    e.preventDefault();
+    componentDidUpdate(prevProps, prevState) {
+        localStorage.setItem(
+            'Form Fields',
+            JSON.stringify(this.state.formFields)
+        );
+        localStorage.setItem('Bearer Token', JSON.stringify(this.state.token));
+    }
 
-    // Make a post request to Ushahidi sever
-    fetch(baseUrl + "/api/v3/posts", {
-      method: "POST",
-      body: JSON.stringify(this.state.formData),
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${this.state.token.access_token}`
-      }
-    })
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        console.log(`Success!!! \n ${data}`);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    // Handler html For when the submit button is pressed
+    onSubmit = e => {
+        e.preventDefault();
 
-    this.setState({
-      formData: {}
-    });
-  };
+        // Make a post request to Ushahidi sever
+        fetch(baseUrl + '/api/v3/posts', {
+            method: 'POST',
+            body: JSON.stringify(this.state.formData),
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${this.state.token.access_token}`
+            }
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(`Success!!! \n ${data}`);
+            })
+            .catch(error => {
+                console.log(error);
+            });
 
-  // Create an array of elements to be rendered in the form
-  FormInputs = props => {
-    return props.fields.map(field => {
-      return (
-        <div key={field.key}>
-          <label htmlFor={field.label}>{field.label}</label>
-          <input
-            type={field.input}
-            name={field.label}
-            placeholder={`Enter the ${field.label}`}
-            value={this.state.formData[field.label]}
-            onChange={e => this.change(e)}
-          />
-        </div>
-      );
-    });
-  };
+        this.setState({
+            formData: {}
+        });
+    };
 
-  render() {
-    return (
-      <form>
-        <this.FormInputs fields={this.state.formFields} />
-        <div>
-          <button onClick={e => this.onSubmit(e)}>Submit</button>
-        </div>
-      </form>
-    );
-  }
+    // Create an array of elements to be rendered in the form
+    FormInputs = props => {
+        return props.fields.map(field => {
+            return (
+                <div key={field.key}>
+                    <label htmlFor={field.label}> {field.label} </label>
+                    <input
+                        type={field.input}
+                        name={field.label}
+                        placeholder={`Enter the ${field.label}`}
+                        value={this.state.formData[field.label]}
+                        onChange={e => this.change(e)}
+                    />
+                </div>
+            );
+        });
+    };
+
+    render() {
+        return (
+            <form>
+                <this.FormInputs fields={this.state.formFields} />
+                <div>
+                    <button onClick={e => this.onSubmit(e)}>Submit</button>
+                </div>
+            </form>
+        );
+    }
 }
