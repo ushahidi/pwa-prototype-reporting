3f0b91bfc10d169ea43bbe1ec932c9f647dd07ea
Nidhi Kumari <nidhi1691996@outlook.com>
2019-08-08 03:15:08 +0530

    added offline support to pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,41 @@
 const { parsed: localEnv } = require('dotenv').config()
+const withOffline = require('next-offline')
 const webpack = require('webpack')
-module.exports = {
+const nextConfig = {
+  generateInDevMode: true,
+  workboxOpts: {
+    // importsDirectory: 'static', // puts workbox in .next/static
+    // importWorkboxFrom: 'local',
+    runtimeCaching: [
+      {
+        urlPattern: /^http[s|]?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'offlineCache',
+          expiration: {
+            maxEntries: 200
+          }
+        }
+      },
+      {
+        urlPattern: /api/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheableResponse: {
+            statuses: [0, 200, 204],
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }
+        }
+      },
+    ]
+  },
   webpack: (config) => {
     config.plugins.push(
       new webpack.EnvironmentPlugin(localEnv)
     )
     return config
   }
-}
+};
+module.exports = withOffline(nextConfig);
